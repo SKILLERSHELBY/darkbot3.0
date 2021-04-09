76d2aeefe99d0b1d3f3b3e15db78ca91e614fbac
SKILLERSHELBY <80931427+SKILLERSHELBY@users.noreply.github.com>
2021-04-08 21:44:06 -0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -77,8 +77,8 @@ cd $HOME
 pkg update && pkg upgrade
 pkg install git
 pkg install wget
-git clone https://github.com/Darkziinh/bot4.0
-cd bot4.0
+git clone https://github.com/SKILLERSHELBY/2
+cd 2
 bash install.sh
 sleep 2
 echo ""
